c9671b33c6b89ec65c82c680d58393c920b2cdc7
Kittinan Samakpong <joker.use.3@gmail.com>
2023-03-18 22:25:40 +0700

    i framer motion, react-respon, tailwind-scrollbar, update sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+import { NavLink, useLocation, useRoutes } from "react-router-dom";
+import SubMenu from "./SubMenu";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 import Logo from "../../assets/logo.png";
 import HomeIcon from "@mui/icons-material/Home";
 import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
@@ -6,128 +12,134 @@ import InsertChartIcon from "@mui/icons-material/InsertChart";
 import HistoryIcon from "@mui/icons-material/History";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Sidebar = () => {
+  let isTabletMid = useMediaQuery({ query: "(max-width: 768px)" });
+  const [open, setOpen] = useState(isTabletMid ? false : true);
+  const sidebarRef = useRef();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (isTabletMid) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  }, [isTabletMid]);
+
+  useEffect(() => {
+    isTabletMid && setOpen(false);
+  }, [pathname]);
+
+  const Nav_animation = isTabletMid
+    ? {
+        open: {
+          x: 0,
+          width: "16rem",
+          transition: {
+            damping: 40,
+          },
+        },
+        closed: {
+          x: -250,
+          width: 0,
+          transition: {
+            damping: 40,
+            delay: 0.15,
+          },
+        },
+      }
+    : {
+        open: {
+          width: "16rem",
+          transition: {
+            damping: 40,
+          },
+        },
+        closed: {
+          width: "4rem",
+          transition: {
+            damping: 40,
+          },
+        },
+      };
+
+  const subMenusList = [
+    {
+      name: "จัดการครุภัณฑ์",
+      icon: DevicesOtherIcon,
+      menus: ["ครุภัณฑ์ทั้งหมด", "ตรวจเช็คครุภัณฑ์"],
+    },
+  ];
+
   return (
     <div class="h-screen border border-r border-gray-200 w-64">
-      <div class="">
-        <img src={Logo} alt="sci-kmutt" />
-        <div class="px-6 pt-4">
-          <ul class="flex flex-col space-y-2">
-            <li class="relative text-gray-500 hover:text-white focus-within:text-white">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                <HomeIcon />
-              </div>
-              <a
-                href="#"
-                class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-              >
+      <div
+        onClick={() => setOpen(false)}
+        className={`md:hidden fixed inset-0 max-h-screen z-[998] bg-black/50 ${
+          open ? "block" : "hidden"
+        } `}
+      ></div>
+      <motion.div
+        ref={sidebarRef}
+        variants={Nav_animation}
+        initial={{ x: isTabletMid ? -250 : 0 }}
+        animate={open ? "open" : "closed"}
+        className=" bg-white text-gray shadow-xl z-[999] max-w-[16rem]  w-[16rem]
+            overflow-hidden md:relative fixed
+         h-screen "
+      >
+        <div className="flex items-center">
+          <img src={Logo} width="100%" alt="sci-kmutt" />
+        </div>
+
+        <div className="flex flex-col  h-full">
+          <ul className="whitespace-pre px-2.5 pt-16 text-[0.9rem] py-5 flex flex-col gap-1  font-medium overflow-x-hidden scrollbar-thin scrollbar-track-white scrollbar-thumb-slate-100   md:h-[68%] h-[70%]">
+            <li class="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 hover:bg-kmuttColor-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+              <NavLink to={"/"} className="link">
+                <HomeIcon size={23} className="min-w-max" />
                 หน้าแรก
-              </a>
+              </NavLink>
             </li>
-            <li class="">
-              <div class="relative flex justify-between text-gray-500 hover:text-white focus-within:text-white">
-                <div class="flex items-center w-full">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                    <DevicesOtherIcon />
+            {(open || isTabletMid) && (
+              <div className="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+                {subMenusList?.map((menu) => (
+                  <div key={menu.name} className="flex flex-col gap-1">
+                    <SubMenu data={menu} />
                   </div>
-                  <a
-                    href="#"
-                    class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-                  >
-                    จัดการครุภัณฑ์
-                  </a>
-                </div>
-                <button
-                  class="absolute right-0 flex items-center p-1 rotate-0"
-                  tabindex="-1"
-                  id="arrow"
-                >
-                  <svg
-                    class="w-5 h-5 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M15.25 10.75L12 14.25L8.75 10.75"
-                    ></path>
-                  </svg>
-                </button>
+                ))}
               </div>
-              <div class="pt-2 pl-4">
-                <ul
-                  class="flex flex-col pl-2 text-gray-500 border-l border-gray-700"
-                  id="submenu"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      class="inline-block w-full px-4 py-2 text-xs rounded hover:bg-kmuttColor-800 hover:text-kmuttColor-500 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:text-kmuttColor-800"
-                    >
-                      ครุภัณฑ์ทั้งหมด
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      class="inline-block w-full px-4 py-2 text-xs rounded hover:bg-kmuttColor-800 hover:text-kmuttColor-500 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:text-kmuttColor-800"
-                    >
-                      ตรวจเช็คครุภัณฑ์
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="relative text-gray-500 hover:text-white focus-within:text-white">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                <InsertChartIcon />
-              </div>
-              <a
-                href="#"
-                class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-              >
+            )}
+            <li class="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 hover:bg-kmuttColor-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+              <NavLink to={"/authentication"} className="link">
+                <InsertChartIcon size={23} className="min-w-max" />
                 สรุปรวม
-              </a>
+              </NavLink>
             </li>
-            <li class="relative text-gray-500 hover:text-white focus-within:text-white">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                <HistoryIcon />
-              </div>
-              <a
-                href="#"
-                class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-              >
+            <li class="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 hover:bg-kmuttColor-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+              <NavLink to={"/stroage"} className="link">
+                <HistoryIcon size={23} className="min-w-max" />
                 ประวัติการยืม
-              </a>
+              </NavLink>
             </li>
-            <li class="relative text-gray-500 hover:text-white focus-within:text-white">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                <AccountBoxIcon />
-              </div>
-              <a
-                href="#"
-                class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-              >
+            <li class="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 hover:bg-kmuttColor-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+              <NavLink to={"/stroage"} className="link">
+                <AccountBoxIcon size={23} className="min-w-max" />
                 ข้อมูลผู้ยืม
-              </a>
+              </NavLink>
             </li>
-            <li class="relative text-gray-500 hover:text-white focus-within:text-white">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-1.5 pointer-events-none">
-                <AdminPanelSettingsIcon />
-              </div>
-              <a
-                href="#"
-                class="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-kmuttColor-800 focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800"
-              >
+            <li class="inline-block w-full py-1 pl-8 pr-4 text-base rounded text-gray-500 hover:bg-kmuttColor-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-kmuttColor-500 focus:bg-kmuttColor-800">
+              <NavLink to={"/stroage"} className="link">
+                <AdminPanelSettingsIcon size={23} className="min-w-max" />
                 แอดมิน
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
+      </motion.div>
+      <div className="m-3 md:hidden  " onClick={() => setOpen(true)}>
+        <MenuIcon size={25} />
       </div>
     </div>
   );
--- client/src/pages/layout/index.jsx
+++ client/src/pages/layout/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Box, useMediaQuery } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import Navbar from "components/Navbar.jsx";
+
+import Navbar from "components/Navbar/Navbar.jsx";
 import Sidebar from "../../components/Sidebar/index.jsx";
 
 const Layout = () => {
