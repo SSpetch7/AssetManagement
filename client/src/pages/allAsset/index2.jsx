2533e36cb59890ace66ae6e7c79f041e6be40b5c
Kittinan Samakpong <joker.use.3@gmail.com>
2023-03-30 01:49:14 +0700

    table useState data

--- client/src/pages/allAsset/index2.jsx
+++ client/src/pages/allAsset/index2.jsx
@@ -1,167 +1,24 @@
-import React, { useMemo, useState } from 'react';
-import MaterialReactTable from 'material-react-table';
-import Pagination from '@mui/material/Pagination';
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+// import { ProductService } from './service/ProductService';
+import { dataTable } from '../../assets/dummy';
 
-const data = [
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
+export default function BasicDemo() {
+  const [products, setProducts] = useState(dataTable);
 
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
-  },
-];
-
-const AllAsset = () => {
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
-      },
-      {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
-      },
-      {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-      },
-    ],
-    []
-  );
+  //   useEffect(() => {
+  //     dataTable.getProductsMini().then((data) => setProducts(data));
+  //   }, []);
 
   return (
-    <div className="mt-12">
-      <div className=" pb-10">
-        <span className="pl-32 font-bold  text-4xl text-gray-600 items-start">
-          All Asset
-        </span>
-        <span className="text-gray-400  pl-2">ครุภัณฑ์ทั้งหมด</span>
-        <div className="flex justify-center h-full ">
-          <div className=" bg-white h-5/6 rounded-xl w-9/12  px-8 pt-8 m-3 ">
-            <MaterialReactTable
-              columns={columns}
-              data={data}
-              enableBottomToolbar={false}
-              enableTopToolbar={false}
-              enableColumnActions={false}
-              initialState={{
-                showGlobalFilter: true,
-              }}
-              muiTablePaginationProps={{
-                rowsPerPageOptions: false,
-              }}
-              muiTableContainerProps={{
-                sx: { maxHeight: '600px', minHeight: '600px' },
-              }}
-            />
-          </div>
-        </div>
-      </div>
+    <div className="card">
+      <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+        <Column field="code" header="Code"></Column>
+        <Column field="name" header="Name"></Column>
+        <Column field="category" header="Category"></Column>
+        <Column field="quantity" header="Quantity"></Column>
+      </DataTable>
     </div>
   );
-};
-
-export default AllAsset;
+}
--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProductService, dataTable } from '../../assets/dummy';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+export default function BasicDemo() {
+  const [products, setProducts] = useState([]);
 
-export default Dashboard;
+  useEffect(() => {
+    // ProductService.getProducts().then((data) => setProducts(data));
+    dataTable.getDatas().then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="card">
+      <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+        <Column field="order" header="ลำดับ"></Column>
+        <Column field="asset_id" header="หมายเลขครุภัณฑ์"></Column>
+        <Column field="name" header="ชื่อรายการ"></Column>
+        <Column field="year" header="ปี"></Column>
+      </DataTable>
+    </div>
+  );
+}
