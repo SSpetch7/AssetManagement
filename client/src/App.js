7366d9b32991ef94b44289ba6fa596234c2873d6
pboss31 <73351605+pboss31@users.noreply.github.com>
2023-05-20 22:29:24 +0700

    test new routes remove navbar sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { Logo } from './assets/logo.png';
 import { Tooltip } from '@mui/material';
 import { Navbar, Sidebar } from './components';
@@ -31,60 +31,60 @@ import 'primeicons/primeicons.css';
 import ForgotPassword from 'pages/forgotpassword';
 import ResetPassword from 'pages/resetpassword';
 
-const App = () => {
+const Layout = ({ children }) => {
   const { activeMenu } = useStateContext();
 
   return (
     <div>
+      {activeMenu ? (
+        <div className="fixed w-72 labtop:w-62 h-full sidebar bg-second-bg">
+          <Sidebar />
+        </div>
+      ) : (
+        <div className="w-0">
+          <Sidebar />
+        </div>
+      )}
+      <div
+        className={`min-h-screen w-full ${
+          activeMenu ? 'bg-main-bg labtop:ml-62 md:ml-72 ' : 'bg-main-bg flex-2'
+        }`}
+      >
+        <div className="fixed md:static bg-main-bg navbar w-full">
+          <Navbar />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
       <Router>
         <div className="flex relative">
-          {activeMenu ? (
-            <div className="fixed  w-72 labtop:w-62 h-full  sidebar bg-second-bg">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0">
-              <Sidebar />
-            </div>
-          )}
-          <div
-            className={`min-h-screen w-full ${
-              activeMenu
-                ? '  bg-main-bg labtop:ml-62 md:ml-72 '
-                : 'bg-main-bg flex-2'
-            }`}
-          >
-            <div className="fixed md:static bg-main-bg navbar w-full">
-              <Navbar />
-            </div>
-
-            <div>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/asset/allAsset" element={<AllAsset />} />
-                <Route path="/asset/eachRoom" element={<EachRoom />} />
-                <Route path="/AssetManage/add" element={<Addnew />} />
-                <Route path="/AssetManage/remove" element={<Remove />} />
-                <Route path="/AssetManage/edit" element={<Edit />} />
-                <Route path="/checkAsset" element={<CheckAsset />} />
-                <Route path="/assetSearching" element={<Rearch />} />
-                <Route path="/report" element={<Report />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/borrow/borrowAsset" element={<Borrowed />} />
-                <Route
-                  path="/borrow/borrowHistory"
-                  element={<BorrowHistory />}
-                />
-                <Route path="/borrow/borrower" element={<Borrower />} />
-                <Route path="/admin" element={<Admin />} />
-              </Routes>
-            </div>
-          </div>
+          <Routes>
+            <Route path="/" element={<Layout><Outlet /></Layout>} />
+            <Route path="/home" element={<Layout><Home /></Layout>} />
+            <Route path="/asset/allAsset" element={<Layout><AllAsset /></Layout>} />
+            <Route path="/asset/eachRoom" element={<Layout><EachRoom /></Layout>} />
+            <Route path="/AssetManage/add" element={<Layout><Addnew /></Layout>} />
+            <Route path="/AssetManage/remove" element={<Layout><Remove /></Layout>} />
+            <Route path="/AssetManage/edit" element={<Layout><Edit /></Layout>} />
+            <Route path="/checkAsset" element={<Layout><CheckAsset /></Layout>} />
+            <Route path="/assetSearching" element={<Layout><Rearch /></Layout>} />
+            <Route path="/report" element={<Layout><Report /></Layout>} />
+            <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/borrow/borrowAsset" element={<Layout><Borrowed /></Layout>} />
+            <Route path="/borrow/borrowHistory" element={<Layout><BorrowHistory /></Layout>} />
+            <Route path="/borrow/borrower" element={<Layout><Borrower /></Layout>} />
+            <Route path="/admin" element={<Layout><Admin /></Layout>} />
+          </Routes>
         </div>
       </Router>
     </div>
